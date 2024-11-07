dcd45e01da766d9ad82936a47a071c2e4b8146d4
cameron <cjcombariza@loyola.edu>
2024-11-07 17:14:27 -0500

    i think this is right

--- main.py
+++ main.py
@@ -1,1 +1,65 @@
-# Code goes here and DO NOT FORGET INTRO COMMENTS
+
+
+#Purpose: creates a circle using *
+#Parameters: none
+#Return: none
+def ASCII_circle():
+    print("\n     ****\n    *      *\n   *        *\n   *        *\n    *      *\n      ****")
+
+
+
+#Purpose: creates a set of lines using *
+#Parameters: none
+#Return: none
+def ASCII_lines():
+    print("\n    |     |     |\n    |     |     |\n    |     |     |")
+
+#Purpose: creates an emoji using *
+#Parameters: none
+#Return: none
+
+def ASCII_emoji():
+    print("\n ¯\_(ツ)_/¯ ")
+
+
+#Purpose: creates a ??? using *
+#Parameters: none
+#Return: none
+def ASCII_word():
+    print("\n*******   ***  ***   ******   ********  ***  ***  ***  ***  ***\n********  ***  ***  ********  ********  **** ***  ***  ***  ***\n***  ***  ***  ***  ***  ***  ***       ********  ***  ***  ***\n***  ***  ***  ***  ***  ***  ***       ********  ***  ***  ***\n*******   ********  ***  ***  ******    *** ****  ***   ****** \n******    ********  ***  ***  ******    ***  ***  ***    ****  \n***       ***  ***  ***  ***  ***       ***  ***  ***  ***  ***\n***       ***  ***  ***  ***  ***       ***  ***  ***  ***  ***\n***       ***  ***  ********  ********  ***  ***  ***  ***  ***\n***       ***  ***   ******   ********  ***  ***  ***  ***  ***")
+
+
+# Purpose: creates a batman symbol using *
+# Parameters: none
+# Return: none
+
+def ASCII_batman():
+    print("\n                   ***********************\n               *********************************\n           *******   *     *       *    *    *******\n        *******   ***      **     **     ***   *******\n      ******   *****       *********      *****    *****\n    ******  ********       *********       ******    *****\n   ****   **********       *********       *********   *****\n  ****  **************    ***********     ************   ****\n ****  *************************************************  ****\n****  ***************************************************  ****\n****  ****************************************************  ****\n****  ****************************************************  ****\n ****  ***************************************************  ****\n  ****  *******     ****  ***********  ****     *********  ****\n   ****   *****      *      *******      *      ********  ****\n    *****   ****             *****             ******   *****\n      *****   **              ***              **    ******\n       ******   *              *              *   *******\n         *******                                *******\n            ********                         *******\n               *********************************\n                    ***********************")
+#found from https://www.asciiart.eu/comics/batman, author did not name themselves posted to this ascii art
+
+#Purpose: call functions
+#Parameters: none
+#Return: none
+
+def main():
+    while True:
+        print("\nPlease choose an option:")
+        print("1. Output a circle, 2. set of lines, 3. an emoji, 4. a word, 5. batman symbol 6. exit")
+
+        choice = input("Enter your choice (1-4): ")
+
+        if choice == '1':
+            ASCII_circle()
+        elif choice == '2':
+            ASCII_lines()
+        elif choice == '3':
+            ASCII_emoji()
+        elif choice == '4':
+            ASCII_word()
+        elif choice == '5':
+            ASCII_batman()
+        elif choice == '6':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice, please select a number between 1 and 4.")
--- reflection.md
+++ reflection.md
@@ -1,9 +1,23 @@
 # Reflection Document
 
-* Full Name :  
-* Student ID:  
+* Full Name :  Cameron Combariza
+* Student ID:  1905032
 
 
+What were you supposed to learn/accomplish? Practice with protecting against bad user input. Practice with decision making. An opportunity to use looping. Practice with designing functions. Practice with implementing functions. A chance to be creative
+
+
+What steps were followed and what techniques did you use to solve the problem?
+What were the Key concepts explored? yes
+
+Did your results match what you expected to get? yes but i took a while to do
+
+
+What challenges did you encounter? figuring out spacing for everything
+How did you follow the first 3 rules of programming? yes
+Did you overcome them, and how? yes i took my time and figured it out
+Do you think you learned what you were supposed to learn for this lab? yes
+What was it like working by yourself? hard to remember to actually do the assignment, as i had no one else who need me to do work.
 
 
 
